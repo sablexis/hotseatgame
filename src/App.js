451cea1450b41836d5eafcf641d181e7c5052daf
Teen Witch Dev <sabrina.a.sandy@gmail.com>
2023-02-23 16:40:02 -0500

    :tada: initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+      <style>
+          @import url('https://fonts.googleapis.com/css2?family=Gaegu:wght@300;400;700&family=Nunito+Sans:ital,wght@0,400;0,600;0,700;0,800;1,600;1,700;1,900&display=swap');
+      </style>
+
+        <div class="card-holder">
+          <div class = "rules card">
+              <h5>Hot Seat:<br></br> The Drinking Game 🔥</h5>
+          </div>
+
+          <div class = "cards card">
+            
+          </div>
+
+        </div>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
